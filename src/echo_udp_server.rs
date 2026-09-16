a0ef9dd2217abf8fe71e480cb8068397f4bc10d7
Christin6 <94294443+Christin6@users.noreply.github.com>
2026-09-16 22:12:36 +1000

    make sure client can receive return message from server

--- src/echo_udp_server.rs
+++ src/echo_udp_server.rs
@@ -10,6 +10,8 @@ pub fn echo_udp_server(port: u16) -> Result<(), Box<dyn std::error::Error>> {
 
     let mut buffer: [MaybeUninit<u8>; 512] = [MaybeUninit::uninit(); 512];
 
+    let echo_message = "Hello, UDP client!";
+
     loop {
         match socket.recv_from(&mut buffer) {
             Ok((n, addr)) => {
@@ -17,7 +19,15 @@ pub fn echo_udp_server(port: u16) -> Result<(), Box<dyn std::error::Error>> {
                     std::slice::from_raw_parts(buffer.as_ptr() as *const u8, n)
                 };
                 let message = String::from_utf8_lossy(bytes);
-                println!("Received from {}: {}", addr.as_socket().unwrap(), message);
+                println!("Received from {}: {}", &addr.as_socket().unwrap(), message);
+
+                let addr_as_string = addr.as_socket().unwrap().to_string();
+
+                // Echo the message back to the client
+                match socket.send_to(echo_message.as_bytes(), &addr.into()) {
+                    Ok(_) => println!("Message echoed back to {}", &addr_as_string),
+                    Err(e) => println!("Failed to echo message: {}", e),
+                }
             }
             Err(e) => {
                 println!("Failed to receive data: {}", e);
--- src/udp_client.rs
+++ src/udp_client.rs
@@ -1,5 +1,6 @@
 use socket2::{Domain, Socket, Type};
 use std::net::{SocketAddr, ToSocketAddrs};
+use std::mem::MaybeUninit;
 
 pub fn create_udp_client(host: String, port: u16) -> Result<(), Box<dyn std::error::Error>> {
     let socket: Socket = Socket::new(Domain::IPV4, Type::DGRAM, None)?;
@@ -15,9 +16,28 @@ pub fn create_udp_client(host: String, port: u16) -> Result<(), Box<dyn std::err
 
     let message =  "Hello, UDP server!";
 
-    match socket.send_to(message.as_bytes(), &address.into()) {
-        Ok(_) => println!("Message sent successfully to {}", address),
-        Err(e) => return Err(format!("Failed to send message: {}", e).into()),
+    let mut buffer: [MaybeUninit<u8>; 512] = [MaybeUninit::uninit(); 512];
+
+    match socket.connect(&address.into()) {
+        Ok(_) => {
+            match socket.send(message.as_bytes()) {
+                Ok(_) => {
+                    println!("Message sent successfully to {}", address);
+                    match socket.recv(&mut buffer) {
+                        Ok(n) => {
+                            let bytes = unsafe {
+                                std::slice::from_raw_parts(buffer.as_ptr() as *const u8, n)
+                            };
+                            let response = String::from_utf8_lossy(bytes);
+                            println!("Received response from {}: {}", address, response);
+                        }
+                        Err(e) => return Err(format!("Failed to receive response: {}", e).into()),
+                    }
+                },
+                Err(e) => return Err(format!("Failed to send message: {}", e).into()),
+            }
+        }
+        Err(e) => return Err(format!("Failed to connect to {}: {}", address, e).into()),
     }
 
     Ok(())
