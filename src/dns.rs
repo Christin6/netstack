0a11f3ad8d412a13f7cfb236287f394f5a7e1985
Christin6 <94294443+Christin6@users.noreply.github.com>
2026-09-19 21:13:40 +1000

    finish dns parsing from response

--- src/dns.rs
+++ src/dns.rs
@@ -1,5 +1,5 @@
-use std::time::SystemTime;
 use crate::udp_client;
+use std::time::SystemTime;
 
 // https://www.rfc-editor.org/info/rfc1035/#section-4.1.1
 struct DnsHeader {
@@ -52,7 +52,11 @@ fn hostname_to_bytes(string: &str) -> Result<Vec<u8>, Box<dyn std::error::Error>
     Ok(bytes)
 }
 
-fn question_to_bytes(hostname: &str, qtype: u16, qclass: u16) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+fn question_to_bytes(
+    hostname: &str,
+    qtype: u16,
+    qclass: u16,
+) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
     let mut bytes = hostname_to_bytes(hostname)?;
     bytes.extend_from_slice(&qtype.to_be_bytes());
     bytes.extend_from_slice(&qclass.to_be_bytes());
@@ -95,10 +99,60 @@ pub fn dig(hostname: String) -> Result<(), Box<dyn std::error::Error>> {
     let query = build_dns_query(&hostname)?;
     println!("DNS Query Bytes: {:?}", query);
 
-    let socket: Result<udp_client::UdpSocket, Box<dyn std::error::Error>> = udp_client::create_udp("8.8.8.8".to_owned(), 53);
-    let udp_client::UdpSocket {socket, address} = socket.unwrap();
+    let socket: Result<udp_client::UdpSocket, Box<dyn std::error::Error>> =
+        udp_client::create_udp("8.8.8.8".to_owned(), 53);
+    let udp_client::UdpSocket { socket, address } = socket.unwrap();
     let response = udp_client::send_and_receive(&socket, &address, &query);
-    println!("{:?}", response?);
+
+    let response_vec: Vec<u8> = response?;
+
+    // r code
+    let flags: [u8; 2] = response_vec[2..4]
+        .try_into()
+        .expect("Error slicing response");
+    let r_code: u16 = u16::from_be_bytes(flags) & 0b0000_0000_0000_1111;
+
+    match r_code {
+        0 => println!("Code {}: No error condition", r_code),
+        1 => println!("Code {}: Format error", r_code),
+        2 => println!("Code {}: Server failure", r_code),
+        3 => println!("Code {}: Name error", r_code),
+        4 => println!("Code {}: Not implemented", r_code),
+        5 => println!("Code {}: Refused", r_code),
+        _ => println!("Code {}: Unknown response code", r_code),
+    }
+
+    // ancount
+    let ancount_slice: [u8; 2] = response_vec[6..8]
+        .try_into()
+        .expect("Error slicing response");
+    let ancount: u16 = u16::from_be_bytes(ancount_slice);
+    println!(
+        "Number of ip addresses/records for {}: {}",
+        &hostname, ancount
+    );
+
+    let answer_section_start = 12 + hostname_to_bytes(&hostname)?.len() + 4;
+    
+    // type, should be 1 (IPv4)
+    let type_field: [u8; 2] = response_vec[answer_section_start+2..answer_section_start+4]
+        .try_into()
+        .expect("Error slicing response");
+    let type_value: u16 = u16::from_be_bytes(type_field);
+    match type_value {
+        1 => println!("Ip is type {}: IPv4", type_value),
+        _ => println!("Ip is type {}: tf", type_value)
+    }
+
+    // r data, only give one ip address
+    let ip_bytes: [u8; 4] = response_vec[answer_section_start + 12..answer_section_start + 16]
+        .try_into()
+        .expect("Error slicing response");
+    println!(
+        "Resolved IP address: {}.{}.{}.{}",
+        ip_bytes[0], ip_bytes[1], ip_bytes[2], ip_bytes[3]
+    );
+    // only resolves IP, not SNI so sometimes resolves into parent IP instead of a specific service
 
     Ok(())
 }
